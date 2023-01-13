a523d5d5d64df3e691e6c8529d9025153f7d965e
seif0912 <baddredineseif@gmail.com>
2023-01-13 17:17:02 +0100

    file structure and hero section

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Hero } from '../../sections/Index'
+
+const Home = () => {
+  return (
+    <>
+        <Hero/>
+    </>
+  )
+}
+
+export default Home
---
+++ src/sections/hero/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './hero.css'
+
+const Hero = () => {
+  return (
+    <div class="hero">
+        <div class="container">
+            <div class="box">
+            <h1>Stay curious</h1>
+            <p>Discover stories, thinking, and expertise from writers on any topic.</p>
+            <a href="/register" class="btn-primary">Start reading</a>
+            
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
