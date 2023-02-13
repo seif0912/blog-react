33aa3e51d5d6fa89f2d85fb5f0193f5221fecb07
seif0912 <baddredineseif@gmail.com>
2023-02-13 11:40:13 +0100

    make login and signin private routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Home, Login } from "./pages/Index";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from './sections/Index'
 // import { useState } from "react";
+// import PrivateRoute from "./privateRoute/PrivateRoute";
+import PrivateRouteLogin from "./privateRoute/PrivateRouteLogin";
 import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
@@ -11,8 +13,13 @@ function App() {
       <AuthProvider>
       <Header/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
+          <Route exact path='/' element={<Home/>}/>
+          <Route element={< PrivateRouteLogin />} >
+            <Route path="/login" element={<Login/>} />
+          </Route>
+          <Route element={< PrivateRouteLogin />} >
+            <Route path="/sign-in" element={<Login/>} />
+          </Route>
       </Routes>
       </AuthProvider>
     </Router>
---
+++ src/privateRoute/PrivateRouteLogin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
+
+const PrivateRoute = () => {
+    const {currentUser} = useAuth();
+        return (
+        !currentUser? <Outlet/> : <Navigate to='/'/>
+        ) 
+}
+
+export default PrivateRoute 
