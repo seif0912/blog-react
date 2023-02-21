c6cca0ca22b8659142ac3894d5c48071ce824c5d
seif badreddine <baddredineseif@gmail.com>
2023-02-21 22:54:27 +0100

    hero change if a user is logged in

--- src/sections/hero/Hero.js
+++ src/sections/hero/Hero.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Container } from '../../components/Index'
+import { useAuth } from '../../contexts/AuthContext';
 import './hero.css'
 
 const Hero = () => {
+  const { currentUser } = useAuth()
   return (
     <div className="hero">
         <Container>
             <div className="box">
-            <h1>Stay curious</h1>
-            <p>Discover stories, thinking, and expertise from writers on any topic.</p>
-            <a href="/register" className="btn-primary">Start reading</a>
+              {currentUser?
+                <>
+                  <h1>Stay creative</h1>
+                  <p>Write stories, thinking, and expertise on any topic you're into.</p>
+                  <a href="/write" class="btn-primary">Start writing</a>
+                </>
+              :
+                <>
+                  <h1>Stay curious</h1>
+                  <p>Discover stories, thinking, and expertise from writers on any topic.</p>
+                  <a href="/register" class="btn-primary">Start reading</a>
+                </>
+            }
             
             </div>
         </Container>
