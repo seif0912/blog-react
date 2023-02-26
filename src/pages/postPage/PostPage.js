91981ae0669be0c88ccea13a9d5ee34643d6d7cd
seif badreddine <baddredineseif@gmail.com>
2023-02-26 12:49:50 +0100

    ability to like and dislike a post

--- src/pages/postPage/PostPage.js
+++ src/pages/postPage/PostPage.js
@@ -1,17 +1,21 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { deleteDoc, doc, getDoc, setDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { db } from '../../firebase/firebase-conf';
 import './postPage.css'
 import { Link } from 'react-router-dom';
+import { HiOutlineHeart, HiHeart } from 'react-icons/hi'
+import { useAuth } from '../../contexts/AuthContext';
 
 const PostPage = () => {
+    let {currentUser} = useAuth()
     let { postId } = useParams();
     const [postExistance, setPostExistance] = useState(false)
     const [post, setPost] = useState()
     const [postCreationTime, setPostCreationTime] = useState()
     const [author, setAuthor] = useState()
     const [loading, setLoading] = useState(true)
+    const [liked, setLiked] = useState(false)
     useEffect(()=>{
         let getPost = async ()=>{
           try{
@@ -21,6 +25,11 @@ const PostPage = () => {
                 setPost(docSnap)
                 const authorDoc = doc(db, 'users', docSnap.data().authorId)
                 const authorSnap = await getDoc(authorDoc)
+                const likeDoc = await getDoc(doc(db, 'posts', postId, 'likes', currentUser.uid))
+                if(likeDoc.data()){
+                    setLiked(prev => !prev)
+                }
+                console.log('like',likeDoc.data())
                 setAuthor(authorSnap.data().displayName)
                 setPostCreationTime( docSnap._document.createTime.toTimestamp().toDate().toString())
                 setPostExistance(true)
@@ -34,6 +43,23 @@ const PostPage = () => {
         return getPost
     },[])
     console.log(post)
+
+    // like handler
+    const likeHandler = async() => {
+        setLiked(liked => !liked)
+        console.log('like',post)
+        try{
+            if(liked){
+                console.log('dislike')
+                await deleteDoc(doc(db, 'posts', post.id, 'likes', currentUser.uid));
+            }else{
+                await setDoc(doc(db, 'posts', post.id, 'likes', currentUser.uid),{
+                    name: currentUser.displayName,
+                    accId: currentUser.uid
+                })
+            }
+        }catch(e){console.error(e)}
+    }
 
     return (
         <div className="write-container">
@@ -54,8 +80,8 @@ const PostPage = () => {
                                 <h4 className="name"><Link to={`/profile/${post.data().authorId}`}>  {author}  <span className='arrow'>&gt;</span> </Link> </h4>
                                 <p>  {postCreationTime && postCreationTime}  </p>
                                 </div>
-                                <div className="like">
-                                <i className="fa-regular fa-heart"></i>
+                                <div className="like" onClick={likeHandler}>
+                                    <span className="heart">{liked? <HiHeart/> : <HiOutlineHeart/>}</span>
                                 </div>
                             </div>
                         </div>
