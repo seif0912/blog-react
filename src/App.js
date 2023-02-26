c372e4ae1dac1a3239fe3e05d276e312ef28a02f
seif badreddine <baddredineseif@gmail.com>
2023-02-26 10:09:03 +0100

    post page: fixing layout bugs and get profile data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from "react";
 import { Home, Login, Register, Write, ProfilePage, PostPage } from "./pages/Index";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from './sections/Index'
-// import { useState } from "react";
-// import PrivateRoute from "./privateRoute/PrivateRoute";
 import PrivateRouteLogin from "./privateRoute/PrivateRouteLogin";
 import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
-  // let [isAuth, setIsAuth] = useState(false)
   return (
     <Router>
       <AuthProvider>
