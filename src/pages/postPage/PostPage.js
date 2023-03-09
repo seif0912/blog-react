6c7c4fe271999ffc0727fbe8b2b6c167a2b558dc
seif badreddine <baddredineseif@gmail.com>
2023-03-09 16:45:03 +0100

    fixing bugs: postpage crash when you're logged out

--- src/pages/postPage/PostPage.js
+++ src/pages/postPage/PostPage.js
@@ -11,7 +11,7 @@ import { useNavigate } from 'react-router-dom';
 const PostPage = () => {
     let {currentUser} = useAuth()
     let { postId } = useParams();
-    const [postExistance, setPostExistance] = useState(false)
+    // const [postExistance, setPostExistance] = useState(false)
     const [post, setPost] = useState()
     const [postCreationTime, setPostCreationTime] = useState()
     const [author, setAuthor] = useState()
@@ -20,35 +20,46 @@ const PostPage = () => {
     const [toggleEdit, setToggleEdit] = useState(false)
     const [toggleDelete, setToggleDelete] = useState(false)
     let navigate = useNavigate()
-
+    
     // fetch the post
     useEffect(()=>{
         let getPost = async ()=>{
-          try{
-            const docRef = doc(db, "posts", postId);
-            const docSnap = await getDoc(docRef);
-            if(docSnap){
+            setLoading(true)
+            try{
+                const docRef = doc(db, "posts", postId);
+                const docSnap = await getDoc(docRef);
+                console.log(docSnap.data())
                 setPost(docSnap)
+                setLoading(false)
                 const authorDoc = doc(db, 'users', docSnap.data().authorId)
                 const authorSnap = await getDoc(authorDoc)
-                const likeDoc = await getDoc(doc(db, 'posts', postId, 'likes', currentUser.uid))
-                if(likeDoc.data()){
-                    setLiked(prev => !prev)
-                }
-                console.log('like',likeDoc.data())
                 setAuthor(authorSnap.data().displayName)
                 setPostCreationTime( docSnap._document.createTime.toTimestamp().toDate().toString())
-                setPostExistance(true)
-                setLoading(prev => !prev)
-                return setLoading(false)
+                if(currentUser){
+                    const likeDoc = await getDoc(doc(db, 'posts', postId, 'likes', currentUser.uid))
+                    if(likeDoc.data()){
+                        setLiked(prev => !prev)
+                    }
+                    console.log('like',likeDoc.data())
+                }
+                // setPostExistance(true)
+                // if(docSnap){
+                // }
+            }catch(e){
+                console.log(e);
+                setLoading(false)
             }
-        }catch(e){
-            console.error(e);
-            setLoading(prev => !prev)
-          }
         }
-        return getPost
-    },[postId, toggleEdit, currentUser.uid])
+        let cleanUp = ()=>{
+            // setPostExistance(false)
+            setLoading(false)
+            setAuthor()
+            setLiked(false)
+
+        }
+        getPost()
+        return cleanUp
+    },[postId, toggleEdit, currentUser])
     console.log(post)
 
     // like handler
@@ -105,7 +116,7 @@ const PostPage = () => {
             </div>
             :
                 <>
-                {postExistance? (
+                {post? (
                     <>
                         <div className={`box post ${!toggleEdit? 'active' : ''}`}>
                             <h2>{post.data().title}</h2>
@@ -113,64 +124,78 @@ const PostPage = () => {
                             {/* <p>{post.data().author}</p> */}
                             <div className="info">
                                 <div className="nd">
-                                <h4 className="name"><Link to={`/profile/${post.data().authorId}`}>  {author}  <span className='arrow'>&gt;</span> </Link> </h4>
+                                <h4 className="name"><Link to={`/profile/${post.data().authorId}`}>  {author && author}  <span className='arrow'>&gt;</span> </Link> </h4>
                                 <p>  {postCreationTime && postCreationTime}  </p>
                                 </div>
                                 <div className="like" onClick={likeHandler}>
                                     <span className="heart">{liked? <HiHeart/> : <HiOutlineHeart/>}</span>
                                 </div>
                             </div>
-                            {post.data().authorId === currentUser.uid &&
-                            <>
-                                <div className="post-settings">
-                                    <h3 id="edit-btn" onClick={()=>{setToggleEdit(true)}}>edit</h3>
-                                    <h3 id="delete-btn" onClick={()=>{setToggleDelete(true)}}>delete</h3>
-                                </div>
-                              </>
+                            {
+                                currentUser &&
+                                <>
+                                {/* edit & delete buttons */}
+                                {post.data().authorId === currentUser.uid &&
+                                    <>
+                                        <div className="post-settings">
+                                            <h3 id="edit-btn" onClick={()=>{setToggleEdit(true)}}>edit</h3>
+                                            <h3 id="delete-btn" onClick={()=>{setToggleDelete(true)}}>delete</h3>
+                                        </div>
+                                    </>
+                                }
+                                </>
                             }
+                            
                         </div>
-                        {post.data().authorId === currentUser.uid &&
-                        <>
-                            { toggleEdit &&
-                            <div className={`box post ${toggleEdit? 'active' : ''}`}>
-                                <form action="">
-                                    <div className="col">
-                                    <input ref={editTitleRef} type="text" name="title" placeholder="title" defaultValue={post.data().title} required/>
-                                    </div>
-                                    <div className="col">
-                                    <textarea ref={editBodyRef} name="body" placeholder="what's on your mind,?" defaultValue={ post.data().body } required></textarea>
-                                    </div>
-                                    <div className="col">
-                                    <button onClick={editHandler} className="publish" type="submit">publish</button>
-                                    </div>
-                                </form>
-                                <div className="post-settings">
-                                    <h3 id="cancel-btn" onClick={()=>{setToggleEdit(false)}}>cancel</h3>
-                                </div>
-                            </div>
-                            }
-                        </>
-                        }
-                        {post.data().authorId === currentUser.uid &&
+                        {
+                            currentUser &&
                             <>
-                                {toggleDelete &&
-                                    <div className={`delete ${toggleDelete? 'active' : ''}`}>
-                                        <div className="delete-box">
-                                        <p>are you sure you want to delete this post</p>
-                                        <div className="btns">
-                                            <h3 onClick={()=>{setToggleDelete(false)}}>cancel</h3>
-                                            <h3 onClick={deleteHandler}>delete</h3>
-                                        </div>
+{/* edit layout */}
+{post.data().authorId === currentUser.uid &&
+                                <>
+                                    { toggleEdit &&
+                                    <div className={`box post ${toggleEdit? 'active' : ''}`}>
+                                        <form action="">
+                                            <div className="col">
+                                            <input ref={editTitleRef} type="text" name="title" placeholder="title" defaultValue={post.data().title} required/>
+                                            </div>
+                                            <div className="col">
+                                            <textarea ref={editBodyRef} name="body" placeholder="what's on your mind,?" defaultValue={ post.data().body } required></textarea>
+                                            </div>
+                                            <div className="col">
+                                            <button onClick={editHandler} className="publish" type="submit">publish</button>
+                                            </div>
+                                        </form>
+                                        <div className="post-settings">
+                                            <h3 id="cancel-btn" onClick={()=>{setToggleEdit(false)}}>cancel</h3>
                                         </div>
                                     </div>
-                                
+                                    }
+                                </>
+                                }
+                                {/* delete layout */}
+                                {post.data().authorId === currentUser.uid &&
+                                    <>
+                                        {toggleDelete &&
+                                            <div className={`delete ${toggleDelete? 'active' : ''}`}>
+                                                <div className="delete-box">
+                                                <p>are you sure you want to delete this post</p>
+                                                <div className="btns">
+                                                    <h3 onClick={()=>{setToggleDelete(false)}}>cancel</h3>
+                                                    <h3 onClick={deleteHandler}>delete</h3>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        
+                                        }
+                                    </>
                                 }
                             </>
                         }
                     </>
-                    
                     )
-                    : (
+                    :
+                    (
                         <div className="box post active">
                             <h2 style={{textAlign: 'center'}}>Post not available or doesn't exist</h2>
                         </div>
